50384ba5c5064a81ffb66dd466f281d57f9b81a9
송우인 <sowintildeblue@gmail.com>
2026-01-07 11:27:30 +0900

    B-3 풀이 완료

--- sectionB.py
+++ sectionB.py
@@ -9,20 +9,27 @@ print(members)
 #* gitbrach 사용하여 문제 풀기
 # B-2 : 딕셔너리 여러 개를 리스트로 구성 
 
-# members = [{"name":"블루윈"}, {"age":25}, {"address":"지구"}]
+members = [{"name":"블루윈"}, {"age":25}, {"address":"지구"}]
+print(members)
 
 
-members=[]
-for i in range(3):
-    name = input("이름")
-    age = input("나이")
+# members=[]
+# for i in range(3):
+#     name = input("이름")
+#     age = input("나이")
 
-    member = {
-        "name" : name,
-        "age" : age
-    }
+#     member = {
+#         "name" : name,
+#         "age" : age
+#     }
 
-    members.append(member)
+#     members.append(member)
 
 
 print(members)
+
+
+# B-3 : 리스트 내 첫 번째 딕셔너리 접근
+
+first = members[0]  # 접근 =  읽기
+print(first)    # 출력
