77d9603722193101620e4dab999ca164d271b93f
송우인 <sowintildeblue@gmail.com>
2026-01-07 12:38:40 +0900

    age로 조건 만들기

--- sectionB.py
+++ sectionB.py
@@ -25,11 +25,47 @@ print(members)
 
 #     members.append(member)
 
-
-print(members)
+# print(members)
 
 
 # B-3 : 리스트 내 첫 번째 딕셔너리 접근
 
 first = members[0]  # 접근 =  읽기
 print(first)    # 출력
+
+# B-4 : 리스트 내 딕셔너리의 특정 키 값 접근
+# 키에 접근해서 값 도출
+first = members[0]["name"]
+print(first)
+
+# B-5 : for문으로 딕셔너리 리스트 전체 순회
+for member in members:
+    print(member)
+
+
+# B-6 : 순회하며 특정 키 값만 출력
+
+
+# for member in members:
+#     if "name" in member:
+#         print(member["name"])
+
+members = [
+    {"name": "윈블루", "age": 26},
+    {"name": "윈그린", "age": 27},
+    {"name": "윈퍼플", "age": 28}
+]
+
+for member in members:
+    print(member["name"])
+
+
+# B-6 : 순회하며 특정 조건 딕셔너리만 출력
+# for member in members:
+#     if "블루" in member["name"]:
+#         print(member["age"])
+
+
+for member in members:
+    if member["age"] < 28:
+        print(member["name"])
