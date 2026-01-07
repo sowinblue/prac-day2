99f9f44be956f548d6dd659b38ffbba65ca68b62
송우인 <sowintildeblue@gmail.com>
2026-01-07 10:19:04 +0900

    내용 수정 전

---
+++ sectionB.py
@@ -0,0 +1,12 @@
+# section B - 딕셔너리를 담은 리스트
+# B-1 : 딕셔너리 하나를 리스트에 추가
+
+members = []
+person = {"name":"블루윈","age":25}
+members.append(person)
+print(members)
+
+#* gitbrach 사용하여 문제 풀기
+# B-2 : 딕셔너리 여러 개를 리스트로 구성 
+
+members = [{"name":"블루윈"}, {"age":25}, {"address":"}}]
