739a9f4b5eb1282abe692e1c3e38f150178630b5
송우인 <sowintildeblue@gmail.com>
2026-01-07 14:26:48 +0900

    if 나열 수정

--- sectionC.py
+++ sectionC.py
@@ -1,1 +1,21 @@
 # section C - 조건문 기초 
+# C-1 : if문 단독 사용
+name = "윈블루"
+
+if "윈" in name:
+    print(name)
+
+
+# C-2 : if-else 사용
+age = int(input("나이를 입력하시오"))
+if age < 50:
+    print("50대 이하")
+
+else: 
+    print("50대 이상")
+
+
+# C-3 : if-elif-else 사용
+# if/ elif 둘 중 하나가 작동되면 끝남
+
+
